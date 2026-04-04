660c38d174f2259e9f7d58057b776c6209fdcd34
Gvoz29 <branislavgvozdenov29@gmail.com>
2026-04-04 11:01:09 +0200

    Optimizacija simulatora sledeci korak dokerizacija

--- Simulator/Simulator.py
+++ Simulator/Simulator.py
@@ -1,19 +1,15 @@
-import requests
-import random
-import time
-import json
-import sys
+import random, time, json
 
 
-API_URl= "http://localhost:5000/data"
-
-SIMULATOR_ID = f"sim_{sys.argv[1]}"
 
 
-def generate_data():
+def generate_data(device_num):
+        device_type= "SMART_METER" if device_num<=500 else "RTU_PMU"
         return{
+            
 
-            "device_id": SIMULATOR_ID,
+            "device_id": f"DEV_{device_num:03d}",
+            "device_type": device_type,
             "voltage": round(random.uniform(210,250),2),
             "current": round(random.uniform(5,20),2),
             "temperature": round(random.uniform(20,80),2),
@@ -21,16 +17,18 @@ def generate_data():
 
         }
 
-def send_data():
-        data= generate_data()
-
-        try:
-            response= requests.post(API_URl, json=data)
-            print(f"Sent: {data} | Status: {response.status_code}")
-        except Exception as e:
-            print("Error:", e)
 
 if __name__ == "__main__":
-    while True:
-        send_data()
-        time.sleep(1)
+    try:
+        while True:
+
+            batch= [generate_data(random.randint(1, 560)) for _ in range(100)]
+
+            data= json.dumps(batch, ensure_ascii=False, indent=2)  
+            print(data)
+            time.sleep(random.uniform(0.1, 0.5))
+
+
+
+    except KeyboardInterrupt:
+         print("Kraj simulacije")        
